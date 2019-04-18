3ff7e8e7702c3653f98847f899c7074161f2fda5
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-04-18 10:59:33 -0700

    rudimentary splitting code

---
+++ brightfield2fish/data/split_data.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+from pytorch_learning_tools.utils.hashsplit import hashsplit
+
+
+def split_and_save(
+    csv_path="/allen/aics/modeling/data/brightfield2fish/preprocessed/data_by_images_normalized.csv",
+    split_col="file",
+    save_dir="data",
+    splits={"train": 0.7, "valid": 0.15, "test": 0.15},
+    seed=0,
+):
+
+    df = pd.read_csv(csv_path)
+    splits = hashsplit(df[split_col], splits=splits, salt=seed)
+
+    for k, v in splits.items():
+        df_subset = df.iloc[v, :].reset_index(drop=True)
+        fname = "{}.csv".format(k)
+        fpath = os.path.join(save_dir, fname)
+        df_subset.to_csv(fpath, index=False)
