dfd3be2ea97f5f015af52a4f380fe91229f8bcec
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-04-19 10:36:12 -0700

    add getitem coverage to dset test

--- brightfield2fish/tests/test_dataset.py
+++ brightfield2fish/tests/test_dataset.py
@@ -1,19 +1,43 @@
 import os
+import numpy as np
 import pandas as pd
 
+from aicsimageio import OmeTifWriter
 from brightfield2fish.data.dataset import FishDataframeDatasetTIFF
 
 
-def test_FishDataframeDatasetTIFF():
+def test_FishDataframeDatasetTIFF(channel_content="MYL2", split="valid", data="fake"):
     dirname = os.path.dirname(__file__)
     csv_path = os.path.join(
         os.path.dirname(os.path.dirname(dirname)),
         "data",
-        "data_by_images_normalized.csv",
+        "splits",
+        "{}.csv".format(split),
     )
 
     df = pd.read_csv(csv_path)
 
-    dset = FishDataframeDatasetTIFF(csv_path, channel_content="DNA", csv=True)
+    dset = FishDataframeDatasetTIFF(csv_path, channel_content=channel_content, csv=True)
 
-    assert len(dset) == len(df["file"].unique())
+    assert len(dset) == len(
+        df[df["channel_content"] == channel_content]["file"].unique()
+    )
+
+    if data == "fake":
+        fake_tiff_dir = "fake_tiffs"
+        if not os.path.exists(fake_tiff_dir):
+            os.makedirs(fake_tiff_dir)
+
+        for i, row in dset.df.iterrows():
+            for channel in ("Brightfield", "Target"):
+                fpath = os.path.join(fake_tiff_dir, "{0}_{1}.tiff".format(channel, i))
+                df.loc[i, channel] = fpath
+                arr = np.random.randint(
+                    low=0, high=2 ** 16 - 1, size=(1, 1, 2, 3, 4), dtype=np.uint16
+                )
+                writer = OmeTifWriter(fpath, overwrite_file=True)
+                writer.save(arr)
+
+    j = np.random.randint(len(dset.df))
+    sample = dset[j]
+    assert sample[0].shape == sample[1].shape
