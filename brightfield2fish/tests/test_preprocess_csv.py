4a2436437c337015d18c5432739ff85d48de330d
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-04-19 12:31:55 -0700

    add filter call to csv test

---
+++ brightfield2fish/tests/test_preprocess_csv.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+import pandas as pd
+from brightfield2fish.data.preprocess_csv import find_czis, filter_czis
+
+
+def test_find_and_filter_czis(data="fake"):
+
+    dirname = os.path.dirname(__file__)
+    csv_path = os.path.join(
+        os.path.dirname(os.path.dirname(dirname)), "data", "data.csv"
+    )
+
+    df = pd.read_csv(csv_path)
+    df = df.rename(index=str, columns={"link_to_data": "directory"})
+    df["plate"] = df["plate"].astype(str)
+    df = df.replace({"PRSS35": "PRSS3", "COL2A": "COL2A1"})
+
+    # add CAAX signal to channel info:
+    for i, row in df.iterrows():
+        if row["cell_line"] == "CAAX":
+            df.loc[i, "probe_561"] = "CAAX"
+
+    # add fake czi files
+    if data == "fake":
+
+        for i, row in df.iterrows():
+            df.loc[i, "directory"] = row["directory"].replace(
+                "/allen/aics/microscopy/Data/RnD_Sandbox",
+                os.path.join(os.path.dirname(os.path.dirname(dirname)), "fake_czis"),
+            )
+
+        for directory in df["directory"].unique():
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            fake_fname = "5500000007_40X_20181010_2-Scene-17-P27-B04.czi"
+            fpath = os.path.join(directory, fake_fname)
+            Path(fpath).touch()
+
+    # find the actual czis and filter for ones that are the right shape
+    df = find_czis(df)
+    df = filter_czis(df)
