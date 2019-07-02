8a6e63cb1cc3d343246aa85cff93022b3e63a64f
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-07-02 13:49:42 -0700

    update aicsimageio dep

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ with open("HISTORY.rst") as history_file:
 requirements = [
     "numpy",
     "pandas",
+    "scikit-learn",
     "scipy",
     "torch>=1.0",
     "torchvision",
     "tqdm",
+    "umap",
     "aicsimageio>=0.6.3",
-    "fnet@git+https://github.com/AllenCellModeling/pytorch_fnet@master#egg=fnet",
+    # "fnet@git+https://github.com/AllenCellModeling/pytorch_fnet@master#egg=fnet",
     "pytorch_learning_tools@git+https://github.com/AllenCellModeling/pytorch_learning_tools@master#egg=pytorch_learning_tools",
 ]
 
