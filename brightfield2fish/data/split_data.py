2c63c1f7b46fff39f34bfd18a96f79dbf4f5783d
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-07-09 11:29:12 -0700

    clean up split_data

--- brightfield2fish/data/split_data.py
+++ brightfield2fish/data/split_data.py
@@ -6,9 +6,10 @@ import numpy as np
 import pandas as pd
 
 
-def hashsplit(X, splits={'train': 0.8, 'test': 0.2}, salt=1, N=5):
+def hashsplit(X, splits={"train": 0.8, "test": 0.2}, salt=1, N=5):
     """
-    splits a list of items pseudorandomly (but deterministically) based on the hashes of the items
+    Splits a list of items pseudorandomly (but deterministically) based on the hashes of the items
+
     Args:
         X (list): list of items to be split into non-overlapping groups
         splits = (dict): dict of {name:weight} pairs definiting the desired split
@@ -17,7 +18,7 @@ def hashsplit(X, splits={'train': 0.8, 'test': 0.2}, salt=1, N=5):
     Returns:
         dict of {name:indices} for all names in the input split dict
     Example:
-        >>> hashsplit(list("allen cell institute"), {'train':0.7,'test':0.3}, salt=3, N=8) 
+        >>> hashsplit(list("allen cell institute"), {'train':0.7,'test':0.3}, salt=3, N=8)
         {'test': [4, 12, 17],
         'train': [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 18, 19]}
     """
@@ -27,13 +28,19 @@ def hashsplit(X, splits={'train': 0.8, 'test': 0.2}, salt=1, N=5):
 
     # determine bins in [0,1] that correspond to each split
     bounds = np.cumsum([0.0] + [v for k, v in sorted(splits.items())])
-    bins = {k: [bounds[i], bounds[i + 1]] for i, (k, v) in enumerate(sorted(splits.items()))}
+    bins = {
+        k: [bounds[i], bounds[i + 1]] for i, (k, v) in enumerate(sorted(splits.items()))
+    }
 
     # hash the strings deterministically
-    hashes = [hashlib.sha512((str(x) + str(salt)).encode('utf-8')).hexdigest() for x in X]
+    hashes = [
+        hashlib.sha512((str(x) + str(salt)).encode("utf-8")).hexdigest() for x in X
+    ]
 
     # create some numbers in [0,1] (at N sig figs) from the hashes
-    nums = np.array([float("".join(filter(str.isdigit, h))[:N]) / 10**N for h in hashes])
+    nums = np.array(
+        [float("".join(filter(str.isdigit, h))[:N]) / 10 ** N for h in hashes]
+    )
 
     # check where the nums fall in [0,1] relative to the bins left and right boundaries
     inds = {k: np.where((nums > l) & (nums <= r)) for k, (l, r) in bins.items()}
@@ -43,14 +50,15 @@ def hashsplit(X, splits={'train': 0.8, 'test': 0.2}, salt=1, N=5):
 
 
 def split_and_save(
-    csv_path="/allen/aics/modeling/data/brightfield2fish/preprocessed/data_by_images_normalized.csv",
+    csv_name="data_by_images_normalized.csv",
+    csv_dir="/allen/aics/modeling/data/brightfield2fish/preprocessed",
     split_col="file",
     save_dir="data/splits",
     splits={"train": 0.7, "valid": 0.15, "test": 0.15},
     seed=0,
 ):
 
-    df = pd.read_csv(csv_path)
+    df = pd.read_csv(os.path.join(csv_dir, csv_name))
     splits = hashsplit(df[split_col], splits=splits, salt=seed)
 
     if not os.path.exists(save_dir):
